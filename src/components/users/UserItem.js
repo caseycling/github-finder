2bc375d4a02b2692f1525872931e825a1841e77f
Casey <caseyclinga@gmail.com>
2020-05-02 18:10:29 -0700

    Refactored class components to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
+//Destructuring user props
+const UserItem = ({ user: { login, avatar_url, html_url }  }) => {
+    // console.log(this.props.user)
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user
-
-        return (
-            <div className="card text-center">
-                <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
-                <h3>{login}</h3>
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+            <h3>{login}</h3>
 s
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default UserItem

51b361e8c74233bd51b513e1152dab87dabd9f51
Casey <caseyclinga@gmail.com>
2020-05-05 14:47:29 -0700

    Created 'clear' button with conditional rendering

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,12 +8,14 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired
     };
 
     onSubmit = e => {
         e.preventDefault();
         this.props.searchUsers(this.state.text);
+
         this.setState({ text: '' });
     }
 
@@ -22,22 +24,27 @@ export class Search extends Component {
     }
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
-                    <input 
-                        type="text" 
-                        name="text" 
-                        placeholder="Search Users..." 
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Search Users..."
                         value={this.state.text}
                         onChange={this.onChange}
                     />
-                    <input 
-                        type="submit" 
-                        value="Search" 
-                        className="btn btn-dark btn-block" 
+                    <input
+                        type="submit"
+                        value="Search"
+                        className="btn btn-dark btn-block"
                     />
                 </form>
+                {showClear &&
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                }
             </div>
         )
     }

b76a91688f2f721d55278ce335c738a5ed3ca403
Casey <caseyclinga@gmail.com>
2020-05-12 11:48:13 -0700

    Removed unnecessary proptypes

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import Repos from '../repos/Repos'
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
-    
+const User = ({ getUserRepos, repos, match }) => {
+    const githubContext = useContext(GithubContext);
+
+    const { user, loading, getUser } = githubContext;
+
     useEffect(() => {
         getUser(match.params.login)
         getUserRepos(match.params.login)
@@ -99,10 +103,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 }
 
 User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
     repo: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired
 }
 

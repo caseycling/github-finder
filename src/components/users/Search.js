fb9fb70b92a7424cc128bec673ede104a51a327d
Casey <caseyclinga@gmail.com>
2020-05-12 11:05:21 -0700

    Remove searchUsers prop

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
     const [text, setText] = useState('');
 
     const onSubmit = e => {
@@ -43,7 +43,6 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
     setAlert: PropTypes.func.isRequired

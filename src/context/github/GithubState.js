0de75c0d2ab3d76e220e025dee4f9f172c0da9c7
Casey <caseyclinga@gmail.com>
2020-05-12 13:08:58 -0700

    Moved getUserRepos function from app to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -55,8 +55,24 @@ const GithubState = props => {
         })
     }
 
+    //Get user repos
+    const getUserRepos = async (username) => {
+        setLoading();
 
-    //Get repos
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+            process.env.REACT_APP_GITHUB_CLIENT_ID
+            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+        )
+        console.log(res)
+
+        //Dispatch to reducer
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        })
+    }
+
 
     //Clear users from state 
     const clearUsers = () => dispatch({ type: CLEAR_USERS })
@@ -71,7 +87,8 @@ const GithubState = props => {
             loading: state.loading,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
         }}
     >
         {props.children}

6c7c460e1c1623630ea4d78d41f59d78ebaf7f29
Casey <caseyclinga@gmail.com>
2020-05-12 11:46:32 -0700

    Cleaned up code and move actions to context

--- src/App.js
+++ src/App.js
@@ -14,27 +14,9 @@ import './App.css';
 
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
   const [repos, setRepos] = useState([])
   const [loading, setLoading] = useState(false)
   const [alert, setAlert] = useState(null)
-
-
-  //Get single Github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
-    )
-
-    //Populate users in state with results 
-    setUser(res.data);
-    setLoading(false);
-  }
 
   //Get user repos
   const getUserRepos = async (username) => {
@@ -49,12 +31,6 @@ const App = () => {
 
     //Populate users repos in state with results 
     setRepos(res.data);
-    setLoading(false);
-  }
-
-  //Clear users from state 
-  const clearUsers = () => {
-    setUsers([]);
     setLoading(false);
   }
 
@@ -76,19 +52,16 @@ const App = () => {
               <Route exact path='/' render={props => (
                 <Fragment>
                   <Search 
-                    clearUsers={clearUsers} 
-                    showClear={users.length > 0 ? true : false} 
                     setAlert={showAlert} 
                   />
                   <Users 
                     loading={loading} 
-                    users={users} 
                   />
                 </Fragment>
               )} />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={getUser} getUserRepos={getUserRepos} user={user} repos={repos} loading={loading} />
+                <User {...props} getUserRepos={getUserRepos} repos={repos} loading={loading} />
               )} />
             </Switch>
           </div>
